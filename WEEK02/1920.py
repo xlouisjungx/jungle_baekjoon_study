5a88e1137cb712ad40a83cb003d415fe4a266f86
xlouisjungx <cutaandcrazy@gmail.com>
2025-03-20 16:43:33 +0900

    공부 시작

---
+++ WEEK02/1920.py
@@ -0,0 +1,13 @@
+# ----- 선언 부분 -----
+
+
+# ----- 문제 해결 부분 -----
+N = int(input())
+
+for i in range(N):
+    numL = list(map(int, input().split()))
+
+comN = int(input())
+
+for i in range(N):
+    comNL = list(map(int, input().split()))
---
+++ WEEK02/README.md
@@ -0,0 +1,7 @@
+# jungle_baekjoon_study
+백준 스터디 및 풀이 공간
+
+### WEEK02
+
+* 1920 수 찾기
+    - [1920.py](1920.py)
