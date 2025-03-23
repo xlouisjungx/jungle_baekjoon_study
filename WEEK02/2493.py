60a741ecd71c91d537aa3e38bfdc1ed87670551f
xlouisjungx <cutaandcrazy@gmail.com>
2025-03-23 22:38:22 +0900

    탑 문제 까지 시도

---
+++ WEEK02/2493.py
@@ -0,0 +1,30 @@
+# ----- 선언 부분 -----
+N = int(input())
+numL = list(map(int, input().split()))
+stack = []
+result = [0] * N
+
+# 예제
+# 5
+# 6 9 5 7 4
+
+# 0 0 2 2 4
+
+# ----- 문제 해결 부분 -----
+
+for i in range(N):
+    while:
+    
+
+'''
+for i in range(N):
+    while stack and numL[stack[-1]] <= numL[i]:
+        stack.pop()
+
+    if stack:
+        result[i] = stack[-1] + 1
+
+    stack.append(i)
+
+print(*result)
+'''
